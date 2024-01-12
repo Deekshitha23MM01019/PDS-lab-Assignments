e71a2026ce62c5e9a8a0edfd2d2139c6451404d2
Deekshitha23MM01019 <155727990+Deekshitha23MM01019@users.noreply.github.com>
2024-01-12 14:01:06 +0530

    Add files via upload

---
+++ 23MM01019_Ass1_ques1.c
@@ -0,0 +1,9 @@
+/*first assignment question 1*/
+#include<stdio.h>
+int main ()
+{ 
+printf("\nName:Pothuganty Deekshitha");
+printf("\nRoll No: 23MM01019");
+printf("\nBranch:Minerals, Metallurgical and Materials Engineering");
+return 0;
+}

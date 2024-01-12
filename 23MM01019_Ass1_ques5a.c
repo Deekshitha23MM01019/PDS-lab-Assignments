8555aef2b87a0eb1fdcf8c85676ee13e53d3ed4f
Deekshitha23MM01019 <155727990+Deekshitha23MM01019@users.noreply.github.com>
2024-01-12 15:33:06 +0530

    Add files via upload

---
+++ 23MM01019_Ass1_ques5a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{ 
+    int a=1,b=12,c=123,d=1234,f=12345;
+    printf("\n 0000%d",a);
+    printf("\n 000%d",b);
+    printf("\n 00%d",c);
+    printf("\n 0%d",d);
+    printf("\n %d",f);
+    return 0;
+}
